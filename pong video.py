1839105e610d73391c34c4fb3af94be53664ac76
rohithardikar <124553640+rohithardikar@users.noreply.github.com>
2023-07-10 20:50:42 +0530

    Add files via upload

---
+++ pong video.py
@@ -0,0 +1,150 @@
+import turtle
+import winsound
+
+wn = turtle.Screen()
+wn.title("Pong Game")
+wn.bgpic("Brick gif.gif")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+ 
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.3
+ball.dy = 0.3
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("violet")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+# line
+line= turtle.Turtle()
+line.speed(0)
+line .shape("square")
+line.color("white")
+line.shapesize(stretch_wid=40,stretch_len=0.03)
+line.penup()
+line.goto(0, 0)
+
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 40
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 40
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 40
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 40
+    paddle_b.sety(y)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
+while True:
+    wn.update()
+    
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+
+    # Top and bottom
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("wall sound",winsound.SND_ASYNC)
+        
+    
+    elif ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("wall sound",winsound.SND_ASYNC)
+
+    # Left and right
+    if ball.xcor() > 350:
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+        winsound.PlaySound("Wrong",winsound.SND_ASYNC)
+
+    elif ball.xcor() < -350:
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+        winsound.PlaySound("Wrong",winsound.SND_ASYNC)
+        
+    # Paddle and ball collisions
+    if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        ball.dx *= -1 
+        winsound.PlaySound("wall sound",winsound.SND_ASYNC)
+    
+    elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+        ball.dx *= -1
+        winsound.PlaySound("wall sound",winsound.SND_ASYNC)
+
+    # Paddles to remain on screen
+    if paddle_a.ycor()>250:
+        paddle_a.sety(250)
+        
+    if paddle_a.ycor()< -250:
+        paddle_a.sety(-250)
+
+    if paddle_b.ycor()>250:
+        paddle_b.sety(250)
+        
+    if paddle_b.ycor()< -250:
+        paddle_b.sety(-250)    
+        
+         
